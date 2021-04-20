5db457ee5a67ff603bff005e5fac91b5aaa7bad8
Jonathan <jtellkampf@gmail.com>
2021-04-20 23:44:36 +0100

    refactored login route to use typescript classes, decorators and metadata using reflect-metadata package

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import { router } from "./routes/loginRoutes";
 import bodyParser from "body-parser";
-import cookieSession from 'cookie-session'
+import cookieSession from "cookie-session";
+import { router as controllerRouter } from "./controllers/decorators/controller";
+import "./controllers/LoginController";
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieSession({ keys: ['jvrnvrnwrn']}))
+app.use(cookieSession({ keys: ["jvrnvrnwrn"] }));
 app.use(router);
+app.use(controllerRouter);
 
 app.listen(3001, () => {
   console.log("Listening on port 3001");
