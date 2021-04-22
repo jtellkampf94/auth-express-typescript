4c4abc3974acb03311d210827783a9e12715a9fc
Jonathan <jtellkampf@gmail.com>
2021-04-22 22:43:21 +0100

    Created use decorator edited controller decorator to account for middleware

---
+++ src/controllers/decorators/use.ts
@@ -0,0 +1,16 @@
+import { MetadataKeys } from "./MetadataKeys";
+import { RequestHandler } from "express";
+import "reflect-metadata";
+
+export function use(middleware: RequestHandler) {
+  return function(target: any, key: string, desc: PropertyDescriptor) {
+    const middlewares =
+      Reflect.getMetadata(MetadataKeys.middleware, target, key) || [];
+
+    Reflect.defineMetadata(
+      MetadataKeys.middleware,
+      [...middlewares, middleware],
+      target
+    );
+  };
+}
